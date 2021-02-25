644d797b09dbd1f2b2b0e63e7f326f925591206f
mrverdant13 <karlo.verde.1996@gmail.com>
2021-02-25 03:55:58 -0500

    Fix migration

--- backend/internal/pkg/database/migrations/mysql/000001_create_departments_table.up.sql
+++ backend/internal/pkg/database/migrations/mysql/000001_create_departments_table.up.sql
@@ -1,9 +1,12 @@
 CREATE TABLE `departments` (
-    `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+    `id` bigint unsigned NOT NULL UNIQUE AUTO_INCREMENT,
+
+    `name` varchar (20) NOT NULL UNIQUE,
+
     `created_at` datetime(3) DEFAULT NULL,
     `updated_at` datetime(3) DEFAULT NULL,
     `deleted_at` datetime(3) DEFAULT NULL,
-    `name` longtext,
+    
     PRIMARY KEY (`id`),
     KEY `idx_departments_deleted_at` (`deleted_at`)
 ); -- ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- backend/internal/pkg/database/migrations/mysql/000002_create_users_table.up.sql
+++ backend/internal/pkg/database/migrations/mysql/000002_create_users_table.up.sql
@@ -1,10 +1,13 @@
 CREATE TABLE `users` (
-    `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+    `id` bigint unsigned NOT NULL UNIQUE AUTO_INCREMENT,
+
+    `email` varchar (127) NOT NULL UNIQUE,
+    `password` varchar (127) NOT NULL,
+
     `created_at` datetime(3) DEFAULT NULL,
     `updated_at` datetime(3) DEFAULT NULL,
     `deleted_at` datetime(3) DEFAULT NULL,
-    `email` longtext,
-    `password` longtext,
+    
     PRIMARY KEY (`id`),
     KEY `idx_users_deleted_at` (`deleted_at`)
 ); -- ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
