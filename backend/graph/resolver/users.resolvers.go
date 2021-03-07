5b6a4364c796e624f887c264e6906d201191c31b
mrverdant13 <karlo.verde.1996@gmail.com>
2021-03-06 19:35:48 -0500

    Allow admin user creation to admin user only

--- backend/graph/resolver/users.resolvers.go
+++ backend/graph/resolver/users.resolvers.go
@@ -5,10 +5,21 @@ package resolver
 
 import (
 	"context"
+	"fmt"
+	"log"
 
 	"github.com/mrverdant13/dash_buttons/backend/graph/gqlmodel"
+	"github.com/mrverdant13/dash_buttons/backend/internal/middlewares"
 )
 
 func (r *mutationResolver) CreateUser(ctx context.Context, input gqlmodel.NewUser) (*gqlmodel.User, error) {
+	if *input.IsAdmin {
+		adminUser := middlewares.CtxAdminUser(ctx)
+		if adminUser == nil {
+			err := fmt.Errorf("Access denied")
+			log.Println(err.Error())
+			return nil, err
+		}
+	}
 	return r.usersRepo.CreateUser(input)
 }
