0de98faac8236a49cb4ecb014fc6bac515ded4c5
mrverdant13 <karlo.verde.1996@gmail.com>
2021-03-09 01:22:17 -0500

    Create base department card

---
+++ frontend-vue/src/components/DepartmentCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <p>
+    {{ department.name }}
+  </p>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+import { Department } from "@/types";
+
+export default defineComponent({
+  name: "DepartmentCard",
+  props: {
+    department: {
+      type: Object as PropType<Department>,
+      required: true,
+    },
+  },
+});
+</script>
+
+
+<style>
+</style>
