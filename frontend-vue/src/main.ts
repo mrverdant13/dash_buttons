c2c92060cf7bbccc0b5d0339fd63ac75758f40e3
mrverdant13 <karlo.verde.1996@gmail.com>
2021-03-10 22:37:01 -0500

    Create and provide default Apollo client

--- frontend-vue/src/main.ts
+++ frontend-vue/src/main.ts
@@ -1,9 +1,25 @@
-import { createApp } from "vue";
+import { createApp, provide, h } from "vue";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App)
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { DefaultApolloClient } from "@vue/apollo-composable";
+
+const defaultApolloClient = new ApolloClient({
+  uri: "http://localhost:4000/query",
+  cache: new InMemoryCache(),
+});
+
+createApp({
+  setup() {
+    provide(DefaultApolloClient, defaultApolloClient);
+  },
+  render() {
+    return h(App);
+  },
+})
   .use(store)
   .use(router)
   .mount("#app");
